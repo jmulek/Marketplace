9264265784a2d4afa7e78f92ebca849d2e7f7bba
Jimmy Mulek <jmulek218@gmail.com>
2016-03-15 23:49:27 -0500

    fixed validation image error

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,9 +8,9 @@ class Listing < ActiveRecord::Base
 										:path => ":style/:id_:filename"
 	
 		
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	end
 	
+	end
+	validates_attachment_content_type :image, content_type:      /\Aimage\/.*\Z/
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: { greater_than: 0 }
 	validates_attachment_presence :image
