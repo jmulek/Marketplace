395d6164f4093bcdb90ae731e2fb34218d6c205a
Jimmy Mulek <jmulek218@gmail.com>
2016-03-16 00:15:15 -0500

    Set user permissions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+	before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
@@ -73,4 +75,23 @@ class ListingsController < ApplicationController
     def listing_params
 			params.require(:listing).permit(:name, :description, :price, :image)
     end
+	def check_user
+		if current_user != @listing_user
+			redirect_to root_url, alert: "Sorry you can't do that."
+		end
+	end
 end
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
