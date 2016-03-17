ce229661b2c6ab45469e3833d59bc2176f70b338
Jimmy Mulek <jmulek218@gmail.com>
2016-03-17 14:15:03 -0500

    fixed errors

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,11 +40,7 @@ class OrdersController < ApplicationController
       flash[:danger] = e.message
     end
 
-    transfer = Stripe::Transfer.create(
-      :amount => (@listing.price * 95).floor,
-      :currency => "usd",
-      :recipient => @seller.recipient
-      )
+
 
     respond_to do |format|
       if @order.save
